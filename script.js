68cb1194fe3179564c26769f3f48787420eba047
Petrus de Souza Matos Pfaffenbach <petrussouza@cp2ejr.com.br>
2025-05-06 15:16:24 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let menu = document.querySelector(".menu");
+let xizinho = document.querySelector(".xizinho");
+
+async function abrirmenu() {
+    menu.style.display = "block";
+    xizinho.style.display = "block";
+}
+
+function fecharmenu() {
+    menu.style.display = "none";
+    xizinho.style.display = "none";
+}
